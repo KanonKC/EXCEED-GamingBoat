6f2fd45d88e85379573174c068fd94d714d389cf
KanonKC <kanonkce@gmail.com>
2022-02-15 20:35:46 +0700

    Init

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div id='navbar' className='p-4'>
+        <p id='project-title'>PROJECT NAME</p>
+    </div>
+  )
+}
+
+export default Navbar
