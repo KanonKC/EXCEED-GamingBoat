3985569c8a72895dda653747217da38caa97b773
natradann <natradaa.w@ku.th>
2022-02-17 00:59:11 +0700

    new

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,13 +2,30 @@ import React from 'react'
 
 const Card = () => {
   return (
-    <div className='card'>
+    <div className='allCard'>
+      <div className='card mx-auto'>
         <div className='day-box'>
             <p className='day mx-8'>M</p>
         </div>
-        <p className='dayName'>Monday</p>
+        <p className='dayName'>MONDAY</p>
+        <p className='time'>18:00</p>
+      </div>
+      <div className='card mx-auto'>
+        <div className='day-box'>
+            <p className='day mx-8'>M</p>
+        </div>
+        <p className='dayName'>MONDAY</p>
         <p className='time'>18:00</p>
     </div>
+    <div className='card mx-auto'>
+        <div className='day-box'>
+            <p className='day mx-8'>M</p>
+        </div>
+        <p className='dayName'>MONDAY</p>
+        <p className='time'>18:00</p>
+    </div>
+    </div>
+    
   )
 }
 
