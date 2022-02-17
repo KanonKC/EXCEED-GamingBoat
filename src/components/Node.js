ba20640b41bfd12a398d4dca0b946f62dcd3f5b9
KanonKC <kanonkce@gmail.com>
2022-02-17 17:46:31 +0700

    Node Decoration

--- src/components/Node.js
+++ src/components/Node.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Node = (props) => {
   return (
-    <div className='node-circle'>
+    <div className='node-circle' style={{
+      "marginRight":`${props.mr}px`
+    }}>
         {props.isChecked>0 && <div className='node-circle-inside mx-auto'></div>}
     </div>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,31 +2,30 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import Edge from '../components/Edge'
 import Node from '../components/Node'
+import { getBoatData } from '../services/boat'
 
-const Home = async () => {
+const Home = () => {
   const [Current,setCurrent] = useState(0)
+  const Meter = [0,450,750,1000]
 
-  
   useEffect(()=>{
-    axios.get('https://ecourse.cpe.ku.ac.th/exceed08/api/get-status').then((response)=>{
-      setCurrent(response.data.passed)
+    getBoatData().then((data)=>{
+      setCurrent(data.passed)
     })
   })
-  
-
-  
-  const Meter = [0,450,750,1000]
 
   return (
     <div>
-        <h1>Home</h1>
-        <div className='flex ml-20'>
-            <Edge bColor="white" w="1000"/>
-            <Edge bColor="#3D5A80" w={Meter[Current]}/>
-            <Node isChecked={true}/>
-            <Node isChecked={Math.floor(Current/1)}/>
-            <Node isChecked={Math.floor(Current/2)}/>
-            <Node isChecked={Math.floor(Current/3)}/>
+        <p className='status mt-10 mb-20'>STATUS: <span className='status-label'>Offline</span></p>
+        <div className=''>
+          <div className='flex path-graph'>
+              <Edge bColor="white" w="1000"/>
+              <Edge bColor="#3D5A80" w={Meter[Current]}/>
+              <Node mr='134' isChecked={true}/>
+              <Node mr='134' isChecked={Math.floor(Current/1)}/>
+              <Node mr='134' isChecked={Math.floor(Current/2)}/>
+              <Node mr='0' isChecked={Math.floor(Current/3)}/>
+          </div>
         </div>
         
     </div>
