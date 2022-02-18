df529ba61d912610de736fb1bc96ef7ce7182d95
N02743 <Utimatentch@gmail.com>
2022-02-18 23:55:23 +0700

    fix layout to figma

--- src/pages/AddTime.js
+++ src/pages/AddTime.js
@@ -6,22 +6,25 @@ const AddTime = () => {
     <div className='popup'>
         <NavbarAddtime/>
         <form className='add-form'>
-            <div className=''>
-                <label>Day : </label>
-                <select className='add-day'>
-                    <option value={'Sunday'}>Sunday</option>
-                    <option value={'Monday'}>Monday</option>
-                    <option value={'Tuesday'}>Tuesday</option>
-                    <option value={'Wednesday'}>Wednesday</option>
-                    <option value={'Thursday'}>Thursday</option>
-                    <option value={'Friday'}>Friday</option>
-                    <option value={'Saturday'}>Saturday</option>
-                </select>
-
-                <label>Time : </label>
-                <input className='add-time' type={"time"}></input>
+            <div className='form-date'>
+                <div className='day-list'>
+                    <label className='add-label'>Day : </label>
+                    <select className='add-day'>
+                        <option value={'Sunday'}>Sunday</option>
+                        <option value={'Monday'}>Monday</option>
+                        <option value={'Tuesday'}>Tuesday</option>
+                        <option value={'Wednesday'}>Wednesday</option>
+                        <option value={'Thursday'}>Thursday</option>
+                        <option value={'Friday'}>Friday</option>
+                        <option value={'Saturday'}>Saturday</option>
+                    </select>
+                </div>
+                <div className='time-list'>
+                    <label className='add-label'>Time : </label>
+                    <input className='add-time' type={"time"}></input>
+                </div>
             </div>
-            <div>
+            <div className='form-btn'>
                 <button className='btn-add add-cancel'>Cancel</button>
                 <button className='btn-add add-save'>Save</button>
             </div>
